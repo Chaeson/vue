967394797f8b7986613a2646e9d3918b0bd821b2
Chae JongWook <only9984@gmail.com>
2020-11-28 23:03:49 +0900

    [ADD]  - Vue 기본 학습 내용 커밋.  - SNS 연동 로그인 개발 진행 중.    > 완료: Google    > 잔여: 카카오 / Apple

---
+++ src/components/login/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Login">
+    <h2>{{this.loginMain}}</h2>
+    <h1>아 이 디</h1>
+    <input type="text">
+    <h1>비밀번호</h1>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data:()=>({
+    loginMain:'로그인 페이지'
+  }),
+  components:{
+  },
+  props:{
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/login/LoginSNS.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="LoginSNS">
+    {{snsTitle}}
+    <vue-kakao-login
+        api-key="<api-key>"
+        image="kakao_login_btn_small"
+        :on-success=onSuccess
+        :on-failure=onFailure
+    />
+    <VueGoogleLogin
+        :params="params"
+        :renderParams="renderParams"
+        :onSuccess="onSuccess"
+        :onFailure="onFailure"
+    >
+    </VueGoogleLogin>
+<!--    <vue-apple-login/>-->
+  </div>
+</template>
+
+<script>
+//import VueAppleLogin from "vue-apple-signin"
+//import VueGoogleLogin from "vue-google-oauth2"
+import VueKakaoLogin from "vue-kakao-login"
+import VueGoogleLogin from 'vue-google-login';
+
+export default {
+  name: "LoginSNS",
+  data:()=>({
+    snsTitle:'SNS 로그인',
+    params:{
+      clientId:'35792323608-v6udmfir0ke6d55udoqmhrei6brmu41t.apps.googleusercontent.com'
+    },
+    renderParams: {
+      width: 250,
+      height: 50,
+      longtitle: true
+    }
+  }),
+  components:{
+    // VueAppleLogin,
+    VueGoogleLogin,
+    VueKakaoLogin
+  },
+  methods:{
+    onSuccess(){
+    },
+    onFailure(){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
